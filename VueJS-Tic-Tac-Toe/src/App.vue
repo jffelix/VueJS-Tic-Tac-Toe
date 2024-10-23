04dcd3c45f4a398bcf9e2cc048bc1f37c0246986
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-10-22 19:57:41 -0700

    Added row files

--- VueJS-Tic-Tac-Toe/src/App.vue
+++ VueJS-Tic-Tac-Toe/src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <h1>VueJS Tic-Tac-Toe</h1>
   </main>
 </template>
 
@@ -17,8 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style scoped>
-header {
+<style>
+/* header {
   line-height: 1.5;
 }
 
@@ -43,5 +38,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
