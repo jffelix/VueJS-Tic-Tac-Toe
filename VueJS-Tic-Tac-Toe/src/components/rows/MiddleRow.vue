03773a37f5fd1c253f76f1d752b7595bc27b5fb4
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-10-24 02:57:28 -0700

    Added B2

--- VueJS-Tic-Tac-Toe/src/components/rows/MiddleRow.vue
+++ VueJS-Tic-Tac-Toe/src/components/rows/MiddleRow.vue
@@ -6,17 +6,20 @@
     <div>
       <h2>Middle Row</h2>
       <RowB1 />
+      <RowB2 />
     </div>
   </main>
 </template>
 
 <script>
 import RowB1 from './subrows/RowB1.vue'
+import RowB2 from './subrows/RowB2.vue'
 
 export default {
     name: 'MiddleRow',
     components: {
-      RowB1
+      RowB1,
+      RowB2
     }
 }
 
---
+++ VueJS-Tic-Tac-Toe/src/components/rows/subrows/RowB2.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        <div className='rowB2'>
+            <h3>B2</h3>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "RowB2"
+}
+</script>
+
+<style>
+.rowB2 {
+    border-style: solid
+}
+</style>
