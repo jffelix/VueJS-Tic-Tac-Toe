abde90680d62acbadd2e554f8b13bff7cbe428b5
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-10-24 02:38:18 -0700

    Added RowA2

--- VueJS-Tic-Tac-Toe/src/components/rows/TopRow.vue
+++ VueJS-Tic-Tac-Toe/src/components/rows/TopRow.vue
@@ -6,17 +6,20 @@
     <div className='topRow'>
         <h2>Top Row</h2>
         <RowA1 />
+        <RowA2 />
     </div>
   </main>
 </template>
 
 <script>
 import RowA1 from './subrows/RowA1.vue'
+import RowA2 from './subrows/RowA2.vue'
 
 export default {
     name: 'TopRow',
     components: {
-        RowA1
+        RowA1,
+        RowA2
     }
 }
 
---
+++ VueJS-Tic-Tac-Toe/src/components/rows/subrows/RowA2.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <div className='rowA2'>
+            <h3>A2</h3>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'RowA2'
+}
+</script>
+
+<style>
+.rowA2 {
+    border-style: solid
+}
+
+</style>
